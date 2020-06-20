43a2d264cb4f2bcfa21c8c77888ff2a7cb0b81d0
Jamesbarford <jamesbarfordevans@gmail.com>
2020-06-20 11:59:02 +0100

    removed un-needed dependencies

--- src/App/Aliens/models/Alien.ts
+++ src/App/Aliens/models/Alien.ts
@@ -1,7 +1,3 @@
-import { immerable } from "immer";
-
 export class Alien {
-    [immerable] = true;
-
     public constructor(public id: string, public scoreValue: number, public isAlive: boolean) {}
 }
